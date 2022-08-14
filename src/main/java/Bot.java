b0aa41b64989987ff8d3f086b0b4d2b768d4de6d
Timur Rakhmatullin <rax_5@mail.ru>
2022-08-15 00:31:35 +0500

    first commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,111 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Bot extends TelegramLongPollingBot {
+    final private String BOT_NAME = "super_calculator_bot";
+
+    Storage storage;
+    ReplyKeyboardMarkup replyKeyboardMarkup;
+
+    Bot()
+    {
+        storage = new Storage();
+        initKeyboard();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        File file = new File("./src/main/resources/token.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        String BOT_TOKEN = sc.nextLine();
+        sc.close();
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try{
+            if(update.hasMessage() && update.getMessage().hasText())
+            {
+                //Извлекаем из объекта сообщение пользователя
+                Message inMess = update.getMessage();
+                //Достаем из inMess id чата пользователя
+                String chatId = inMess.getChatId().toString();
+                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
+                String response = parseMessage(inMess.getText());
+
+                //Создаем объект класса SendMessage - наш будущий ответ пользователю
+                SendMessage outMess = new SendMessage();
+
+                //Добавляем в наше сообщение id чата а также наш ответ
+                outMess.setChatId(chatId);
+                outMess.setText(response);
+
+                outMess.setReplyMarkup(replyKeyboardMarkup);
+
+                //Отправка в чат
+                execute(outMess);
+
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    public String parseMessage(String textMsg) {
+        String response;
+
+        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
+        if(textMsg.equals("/start"))
+            //response = "Приветствую, бот знает много цитат. Жми /get, чтобы получить случайную из них";
+            response = "Hello, this bot remote control smart home";
+        else if(textMsg.equals("/get"))
+            response = storage.getRandQuote();
+        else
+            // = "Сообщение не распознано";
+            response = textMsg;
+        System.out.println(textMsg);
+        return response;
+    }
+
+    void initKeyboard()
+    {
+        //Создаем объект будущей клавиатуры и выставляем нужные настройки
+        replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true); //подгоняем размер
+        replyKeyboardMarkup.setOneTimeKeyboard(false); //скрываем после использования
+
+        //Создаем список с рядами кнопок
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        //Создаем один ряд кнопок и добавляем его в список
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRows.add(keyboardRow);
+        //Добавляем одну кнопку с текстом "Просвяти" наш ряд
+        keyboardRow.add(new KeyboardButton("Blink"));
+        keyboardRow.add(new KeyboardButton("On"));
+        keyboardRow.add(new KeyboardButton("Off"));
+        //добавляем лист с одним рядом кнопок в главный объект
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+    }
+}
